74f068c0576be6b22554fd223e9f42bd417a5af0
marf3927 <xogml3927@naver.com>
2019-08-14 18:11:53 +0900

    package & folder structure/

---
+++ src/blockchain.js
@@ -0,0 +1,9 @@
+class Bolck{
+    constructor(index, hash, priviousHash, timestamp, data){
+        this.index = index;
+        this.hash = hash;
+        this.priviousHash = priviousHash;
+        this.timestamp = timestamp;
+        this.data = data
+    }
+}
